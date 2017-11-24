75bbec622924415bd1d547b479bd57e81a0295b2
PENLAP <PENLAP@DESKTOP-L9M2G5N>
2017-11-24 13:38:11 +0900

    Add project files.

---
+++ SendFile/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SendFile.Startup))]
+namespace SendFile
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
